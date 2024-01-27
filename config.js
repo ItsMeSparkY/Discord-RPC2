89bb41860d50d4c5cdfb6cb7b4f829b3a36152ef
ItsMeSparkY <119124402+ItsMeSparkY@users.noreply.github.com>
2024-01-27 18:23:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,17 +9,17 @@ module.exports = {
   showTime: false, // Toggle to show or hide time in status (true/false)
   token: "" || process.env.TOKEN,
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
-  Name: "Name Here!",
-  State: "State Here!",
-  Details: "Details Here!",
-  FirstButtonName: "Button1 Name",
-  FirstButtonUrl: "https://spicydevs.me/",
-  SecondButtonName: "Button2 Name",
-  SecondButtonUrl: "https://spicydevs.me/",
-  LargeImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453767412621362/c195de24559e92ba8f283a719d2e5137.png", // DISCORD CDN IMAGE ONLY
-  LargeText: "Large Image Text", // hover text for large image
-  SmallImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453635732451429/9b92f310a29c0dbcab36e825cb62593b.png", // DISCORD CDN IMAGE ONLY
-  SmallText: "Small Image Text", // hover text for small image
+  Name: "MoonLight",
+  State: "Reviving",
+  Details: "Legit",
+  FirstButtonName: "Join",
+  FirstButtonUrl: "https://discord.gg/mlontop",
+  SecondButtonName: "Us",
+  SecondButtonUrl: "https://discord.gg/mlontop",
+  LargeImage: "https://cdn.discordapp.com/icons/1187033801914851338/a_f294e752903ea046e2f389712c50f1b2.gif?size=2048", // DISCORD CDN IMAGE ONLY
+  LargeText: "MoonLight", // hover text for large image
+  SmallImage: "https://cdn.discordapp.com/emojis/1134648732604977253.gif?size=128&quality=lossless", // DISCORD CDN IMAGE ONLY
+  SmallText: "legit", // hover text for small image
 };
 
 ///////////////////////////
