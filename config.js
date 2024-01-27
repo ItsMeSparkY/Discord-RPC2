dbbf1148e93568ac85267ec7fd9f298c4c86b995
ItsMeSparkY <119124402+ItsMeSparkY@users.noreply.github.com>
2024-01-27 18:35:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   SecondButtonName: "Us",
   SecondButtonUrl: "https://discord.gg/mlontop",
   LargeImage: "https://cdn.discordapp.com/icons/1187033801914851338/a_f294e752903ea046e2f389712c50f1b2.gif?size=2048", // DISCORD CDN IMAGE ONLY
-  LargeText: "MoonLight", // hover text for large image
+  LargeText: "Free OwO, Ltc, Nitro", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/emojis/1134648732604977253.gif?size=128&quality=lossless", // DISCORD CDN IMAGE ONLY
   SmallText: "legit", // hover text for small image
 };
