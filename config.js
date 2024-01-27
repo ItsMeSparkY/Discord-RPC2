517e177e63b8ed303001d554d6633124ee5a3b4d
ItsMeSparkY <119124402+ItsMeSparkY@users.noreply.github.com>
2024-01-27 19:24:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@
 module.exports = {
   // refer to tutorial1.png and tutorial2.png in the repo for help
   showTime: false, // Toggle to show or hide time in status (true/false)
-  TOKEN = fs.readFileSync('data.txt', 'utf-8').trim();
+  const fs = require('fs');
+const TOKEN = fs.readFileSync('data.txt', 'utf-8').trim();
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
   Name: "MoonLight",
   State: "Nitro, Ltc, OwO",
